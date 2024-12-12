c22e85bd4369313bbde4c6248ad31852fc72334d
Hiroyuki Momoi <hirocorp01@gmail.com>
2024-12-12 22:52:18 +0900

    cleanup

--- frontend/research-trend-analysis/components/custom/navbar.tsx
+++ frontend/research-trend-analysis/components/custom/navbar.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { SlashIcon } from "./icons";
 import { ThemeToggle } from "./theme-toggle";
 
 export const Navbar = async () => {
@@ -8,15 +7,6 @@ export const Navbar = async () => {
       <div className="bg-backgrond absolute top-0 left-0 w-dvw py-2 px-3 justify-between flex flex-row items-center z-30">
         <div className="flex flex-row gap-3 items-center">
           <div className="flex flex-row gap-2 items-center">
-            <Image
-              src="/images/gemini-logo.png"
-              height={20}
-              width={20}
-              alt="Gemini Logo"
-            />
-            <div className="text-zinc-500">
-              <SlashIcon size={16} />
-            </div>
             <div className="text-sm dark:text-zinc-300 truncate w-28 md:w-fit">
               Research Trend Analysis
             </div>
